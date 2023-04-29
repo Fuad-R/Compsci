7d37189ba4f42febba9a9a006ebe2557031ecba0
Fuad <fuadarizik@gmail.com>
2023-04-29 10:36:15 +0200

    Big upgrade

--- src/28_4_2023.java
+++ src/28_4_2023.java
@@ -1,9 +1,12 @@
 import java.util.Scanner;
+import java.text.DecimalFormat;
+import java.math.RoundingMode;
 
 class Test25{
     public static void main(String[]arg)
 {
     Scanner scanner = new Scanner(System.in);
+    DecimalFormat df = new DecimalFormat("0.00");
 
     System.out.print("Enter number of students: ");
     int x = scanner.nextInt();
@@ -14,28 +17,68 @@ class Test25{
 
     String[] student = new String[x];
     int[] grade = new int[x];
+    String[] gradeString = new String[x];
+    String[] passfail = new String[x];
 
     for (int i = 0 ; i < x ; i++){
         System.out.print("Enter name of student number "+(i+1)+":");
         student[i] = scanner.next();
 
-        System.out.print("Enter grade for student number "+(i+1)+":");
+        System.out.print("Enter grade (0-100) for student number "+(i+1)+":");
         grade[i] = scanner.nextInt();
 
     System.out.println();
     }
-    System.out.println("Student      Grade");
+    // System.out.println("Student & Grade");
     for(int i = 0; i < x ; i++ ){
-        System.out.println(student[i]+"            "+grade[i]);
+        // System.out.println(student[i]+" got "+grade[i]);
         sum = sum + grade[i];
 
         if(grade[i] > max){
             max = grade[i];
         }
-    }
-    System.out.println("The class average is "+(sum/x)+" and the maximum grade is "+max);
+        gradeString[i] = String.valueOf(grade[i]);
 
+        if(grade[i]<50){
+            passfail[i] = "\u001B[31m Fail \u001B[37m";
+        } else{
+            passfail[i] = "\u001B[32m Pass \u001B[37m";
+        }
+       
+    }
+        
+    System.out.println("The class average is "+(sum/x)+" and the maximum grade is "+max);
+ 
+    System.out.println();
+
+// final Object[][] table = new String[x][];
+//     for(int i=0 ; i < x ; i++){
+        
+//         table[i] = new String[] { student[i], gradeString[i], passfail[i]};
+//     }
+
+// for (final Object[] row : table) {
+//     System.out.format("%2s%7s%7s%n", row);
+// }
+    System.out.print("\033[H\033[2J");  
+    System.out.flush();  
+    
+    String l = "--------------------------------%n";
+
+    System.out.printf(l);
+    System.out.printf("|       Student results        |%n");
+    System.out.printf(l);
+    
+
+    for(int n = 0; n < x ; n++ ){
+        System.out.printf("| %-10s | %-6s | %-7s |%n", student[n], gradeString[n], passfail[n]);
+    }
     scanner.close();
+    System.out.printf(l);
+    System.out.printf("|         Class Results        |%n");
+    System.out.printf("| %-8s | %-3s | %-3s | %-3s |%n", "Average", df.format((sum/x)), "Max", max);
+    System.out.printf(l);
+
 }
 
 }
