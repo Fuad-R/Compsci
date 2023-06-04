f252733ed2d536194956776e69431cf1dfcb8956
Fuad <fuadarizik@gmail.com>
2023-06-04 17:56:35 +0200

    update

---
+++ src/main3.java
@@ -0,0 +1,9 @@
+public class main3 {
+   public static void main(String[] args) {
+
+    square square = new square(20);
+
+    System.out.println(square.getarea());
+    System.out.println(square.getperimiter());
+}
+}
